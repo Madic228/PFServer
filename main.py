2346751ee819d19b189e5062c1f50b2f51cbe540
Girich Sergey <girich03@gmail.com>
2024-12-27 02:01:32 +0500

    auth

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from routers import fill_news
+from routers import fill_news, auth
 import uvicorn
 
 app = FastAPI()
 
 app.include_router(fill_news.router, prefix="/api/fill_news", tags=["Fill News"])
+app.include_router(auth.router)
 
 if __name__ == "__main__":
     uvicorn.run(
